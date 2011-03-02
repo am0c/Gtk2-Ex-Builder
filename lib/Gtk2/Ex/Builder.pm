b2465f1455b4a02f45df7dc6648fd67bd7d5ddb7
Hojung Yoon <amoc.yn@gmail.com>
2011-03-02 15:26:09 +0900

    embeded a documentation

--- lib/Gtk2/Ex/Builder.pm
+++ lib/Gtk2/Ex/Builder.pm
@@ -135,3 +135,61 @@ sub get_widget {
 
 
 1;
+
+=pod
+
+=head1 NAME
+
+Gtk2::Ex::Builder - Gtk2::Widget Wrapper and Gtk2 Building DSL
+
+=head1 SYNOPSIS
+
+  use Gtk2 -init;
+  use Gtk2::Ex::Builder;
+
+  my $app = builder {
+    meta isa => 'Window';
+    sets title => 'My Application';
+    sets default_size => 400, 400;
+    on delete_event => sub { Gtk2->main_quit };
+
+    hav builder {
+      meta isa => 'Button';
+      meta is => 'my_button';
+      sets label => 'Hello World';
+      on clicked => sub { print "Hi\n" };
+    };
+  };
+
+  print $app->get_widget('my_button')->get_label, "\n";
+
+  Gtk2->main;
+
+=head1 PRE-ALPHA VERSION
+
+This library is totally B<UNDER DEVELOPING>
+and B<APIs COULD BE CHANGED WITHOUT NOTICE> currently.
+
+Any recommendations or criticisms or ideas are welcome.
+
+=head1 DESCRIPTION
+
+L<Gtk2::Ex::Builder> is a Domain-specific Language to
+compose several Gtk2 widgets, and also a wrapper for a Gtk2 widget.
+
+=head1 SUPPORT
+
+=head1 AUTHOR
+
+Hojung Youn <amorette@cpan.org>
+
+The author is on C<irc.freenode.org> in C<#perl-kr>, and on L<http://twitter.com/am0c>.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Hojung Youn.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
